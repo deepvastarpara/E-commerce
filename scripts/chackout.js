9566b89de2f58e1c73109e61f7cadc854d4f0d12
deepvastarpara <deepvastarpara7@gmail.com>
2025-07-05 16:59:32 +0530

    make delivery options interactive

--- scripts/chackout.js
+++ scripts/chackout.js
@@ -1,4 +1,4 @@
-import { cart, removeForCart } from '../data/cart.js';
+import { cart, removeForCart , updateDeliveryOption} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formentCurrency } from './utils/money.js';
 //dyyjs library by default export 
@@ -108,7 +108,9 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html += ` 
-         <div class="delivery-option">
+         <div class="delivery-option js-delivery-option "
+            data-product-id="${matchingProduct.id}"
+            data-delivery-option-id="${deliveryOption.id}">
             <input type="radio" ${isChecked ? 'checked' : ''}
                 class="delivery-option-input"
                 name="delivery-option-${matchingProduct.id}">
@@ -138,6 +140,13 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
             .js-cart-item-container-${productId}`
         );
         container.remove();
+    });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click' , () => {
+        const {productId,deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId);
     });
 });
 
