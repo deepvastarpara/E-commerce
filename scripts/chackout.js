19a10fc56ea2e33ef107459dec7d25194697eae9
deepvastarpara <deepvastarpara7@gmail.com>
2025-07-06 14:29:13 +0530

    create class version of cart

--- scripts/chackout.js
+++ scripts/chackout.js
@@ -1,6 +1,6 @@
 import { renderOrederSummary } from "./chackout/orderSymmary.js";
 import { renderPaymentSummary } from "./chackout/paymentSummary.js";
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 
 renderOrederSummary();
 renderPaymentSummary();
